312045531e7f902f71a1f125cdfa32a9ff393b27
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-06-12 18:40:25 +0530

    storing and fetching data using SQLite

--- lib/providers/great_place.dart
+++ lib/providers/great_place.dart
@@ -1,6 +1,7 @@
 import 'package:Flutter_native_Device_features/models/place.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io';
+import '../helpers/db_helper.dart';
 
 class GreatPlace with ChangeNotifier {
   List<Place> _items = [];
@@ -15,6 +16,25 @@ class GreatPlace with ChangeNotifier {
         title: title,
         location: null);
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insertData('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchPlace() async {
+    final dataList = await DBHelper.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+              id: item['id'],
+              title: item['title'],
+              image: File(item['image']),
+              location: null),
+        )
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/place_list.dart
+++ lib/screens/place_list.dart
@@ -17,21 +17,29 @@ class PlacesList extends StatelessWidget {
               })
         ],
       ),
-      body: Consumer<GreatPlace>(
-          child: Center(
-            child: Text('No places adeed'),
-          ),
-          builder: (context, value, ch) => value.items.length <= 0
-              ? ch
-              : ListView.builder(
-                  itemBuilder: (ctx, i) => ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: FileImage(value.items[i].image),
-                    ),
-                    title: Text(value.items[i].title),
-                  ),
-                  itemCount: value.items.length,
-                )),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlace>(context, listen: false).fetchPlace(),
+        builder: (context, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlace>(
+                child: Center(
+                  child: Text('No places adeed'),
+                ),
+                builder: (context, value, ch) => value.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(value.items[i].image),
+                          ),
+                          title: Text(value.items[i].title),
+                        ),
+                        itemCount: value.items.length,
+                      )),
+      ),
     );
   }
 }
